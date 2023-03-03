a63c9c1c90516af4d40cdf63781dfaf2246e01c9
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2023-03-03 11:09:49 +0100

    updated navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { categories } from "./categories";
+import Logo from "../../assets/logo.svg";
 
 console.log(categories);
 
@@ -7,8 +8,9 @@ const Navbar = () => {
   return (
     <div
       style={{ borderBottom: "1px solid #ccc" }}
-      className="text-customOrange"
+      className="text-customOrange flex py-3"
     >
+      <img src={Logo} />
       Navbar
     </div>
   );
