b80c59ac248c76477b5161b58852888c046a9c46
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2023-03-03 20:41:37 +0100

    updated sidenav

--- src/components/sidenav/Sidenav.jsx
+++ src/components/sidenav/Sidenav.jsx
@@ -7,14 +7,16 @@ const Sidenav = () => {
       className="absolute bg-customBlack border border-red-500 w-full h-full top-0 left-0"
       style={{ opacity: "0.5" }}
     >
-      <div className="bg-white h-full" style={{ width: "70%", opacity: "1" }}>
+      <div
+        className="absolute bg-white h-full p-4"
+        style={{ width: "70%", opacity: "initial" }}
+      >
         <ul>
           {categories.map((x, index) => (
             <li key={index}>{x.title}</li>
           ))}
         </ul>
       </div>
-      <p>Collections</p>
     </div>
   );
 };
