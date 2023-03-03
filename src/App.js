8ff75e620a1521ea403b3e45d6fdd4dac575bf89
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2023-03-03 19:45:16 +0100

    updated header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Navbar from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div style={{ width: "1440px", margin: "0 auto" }}>
-      <Navbar />
+      <Header />
       <h1>Hello World!</h1>
     </div>
   );
