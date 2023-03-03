23fde6b9b1eb028eac40569da89eaab56068f135
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2023-03-03 08:33:23 +0100

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
-    <>
-    <h1>Hello World!</h1>
-    </>
+    <div style={{ width: "1440px", margin: "0 auto" }}>
+      <Navbar />
+      <h1>Hello World!</h1>
+    </div>
   );
 }
 
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () => {
+  return <div style={{ borderBottom: "1px solid #ccc" }}>Navbar</div>;
+};
+
+export default Navbar;
