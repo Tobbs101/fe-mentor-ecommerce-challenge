e10a1b79c78a06c491fa051c15d5d2487542fbcd
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2023-03-03 20:55:54 +0100

    changes to sidenav

--- src/components/sidenav/Sidenav.jsx
+++ src/components/sidenav/Sidenav.jsx
@@ -5,12 +5,15 @@ import Close from "../../assets/icon-close.svg";
 const Sidenav = ({ toggleNav, setToggleNav }) => {
   return (
     <div
-      className="absolute bg-white h-full p-4 top-0 left-0"
+      className="absolute bg-white h-full p-7 top-0 left-0"
       style={{ width: "70%", opacity: "unset", zIndex: "20" }}
     >
+      <img src={Close} alt="close" className="mb-20" />
       <ul>
         {categories.map((x, index) => (
-          <li key={index}>{x.title}</li>
+          <li key={index} className="mb-5 font-bold text-darkBlue text-xl">
+            {x.title}
+          </li>
         ))}
       </ul>
     </div>
