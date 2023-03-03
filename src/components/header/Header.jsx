aea8a4ee17f774efb72896815a772b5ee9a57da2
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2023-03-03 19:57:55 +0100

    changes to header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,14 +4,18 @@ import Logo from "../../assets/logo.svg";
 import Cart from "../../assets/icon-cart.svg";
 import Avatar from "../../assets/image-avatar.png";
 import Menu from "../../assets/icon-menu.svg";
+import useWindowDimensions from "../../hooks/useWindowSize";
 
 const Navbar = () => {
   const [showNavItemID, setShowNavItemID] = useState(null);
+  const { width } = useWindowDimensions();
+
+  console.log(width);
 
   return (
     <div
       style={{ borderBottom: "1px solid #ccc" }}
-      className="flex py-7 items-center justify-between"
+      className="flex py-7 items-center justify-between px-2"
     >
       <div className="flex gap-5 items-center">
         <img src={Menu} alt="menu" />
