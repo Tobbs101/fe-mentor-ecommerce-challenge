eeb01288a11006513092f20eadfdda5dc9e0dc7d
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2023-03-06 13:53:13 +0100

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,83 +8,86 @@ import useWindowDimensions from "../../hooks/useWindowSize";
 import Cart from "../cart/Cart";
 import { CartContext } from "../../context/CartContext";
 
-const Navbar = React.memo(({ toggleNav, setToggleNav }) => {
-  const [showNavItemID, setShowNavItemID] = useState(null);
-  const [showCart, setShowCart] = useState(false);
-  const { width } = useWindowDimensions();
-  const { cartDetails } = useContext(CartContext);
+const Navbar = React.memo(
+  ({ toggleNav, setToggleNav, toggleBackDrop, setToggleBackDrop }) => {
+    const [showNavItemID, setShowNavItemID] = useState(null);
+    const [showCart, setShowCart] = useState(false);
+    const { width } = useWindowDimensions();
+    const { cartDetails } = useContext(CartContext);
 
-  // console.log(width);
-  // console.log(toggleNav);
+    // console.log(width);
+    // console.log(toggleNav);
 
-  console.log(cartDetails);
+    console.log(cartDetails);
 
-  return (
-    <div
-      style={{ borderBottom: "1px solid #e5e5e5" }}
-      className="relative flex py-7 items-center justify-between px-3"
-    >
-      {showCart && <Cart width={width} setShowCart={setShowCart} />}
-      <div className="flex gap-5 items-center">
-        {width < 745 && (
+    return (
+      <div
+        style={{ borderBottom: "1px solid #e5e5e5" }}
+        className="relative flex py-7 items-center justify-between px-3"
+      >
+        {showCart && <Cart width={width} setShowCart={setShowCart} />}
+        <div className="flex gap-5 items-center">
+          {width < 745 && (
+            <button
+              className="cursor-pointer"
+              onClick={() => {
+                setToggleNav(!toggleNav);
+                setToggleBackDrop(!toggleBackDrop);
+              }}
+            >
+              <img src={Menu} alt="menu" />
+            </button>
+          )}
+          <img src={Logo} alt="logo" />
+          {width > 744 && (
+            <ul className="flex gap-8 ml-7">
+              {categories.map((x, index) => (
+                <li
+                  key={index}
+                  value={x.id}
+                  className="text-customGray hover:text-darkBlue relative cursor-pointer"
+                  onMouseEnter={(e) => {
+                    setShowNavItemID(e.target.value);
+                  }}
+                  onMouseLeave={() => {
+                    setShowNavItemID(null);
+                  }}
+                >
+                  {x.title}
+                  {showNavItemID === x.id && (
+                    <span
+                      className="absolute w-full border-2 border-customOrange left-0"
+                      style={{ top: "56px" }}
+                    ></span>
+                  )}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        <div className="flex gap-4 items-center">
           <button
-            className="cursor-pointer"
+            className="relative flex items-center justify-center cursor-pointer hover:bg-slate-100 border border-transparent hover:border-slate-100 h-10 w-10 rounded-full"
             onClick={() => {
-              setToggleNav(!toggleNav);
+              setShowCart(!showCart);
             }}
           >
-            <img src={Menu} alt="menu" />
+            {cartDetails.itemQuantity > 0 && (
+              <div className="absolute top-1 right-0 bg-customOrange text-white border border-customOrange h-3 w-4 flex items-center justify-center p-1 rounded-lg">
+                <p style={{ fontSize: "10px" }}>{cartDetails.itemQuantity}</p>
+              </div>
+            )}
+            <img src={CartIcon} alt="cart" className="" />
           </button>
-        )}
-        <img src={Logo} alt="logo" />
-        {width > 744 && (
-          <ul className="flex gap-8 ml-7">
-            {categories.map((x, index) => (
-              <li
-                key={index}
-                value={x.id}
-                className="text-customGray hover:text-darkBlue relative cursor-pointer"
-                onMouseEnter={(e) => {
-                  setShowNavItemID(e.target.value);
-                }}
-                onMouseLeave={() => {
-                  setShowNavItemID(null);
-                }}
-              >
-                {x.title}
-                {showNavItemID === x.id && (
-                  <span
-                    className="absolute w-full border-2 border-customOrange left-0"
-                    style={{ top: "56px" }}
-                  ></span>
-                )}
-              </li>
-            ))}
-          </ul>
-        )}
+          <img
+            src={Avatar}
+            alt="avatar"
+            className="border-2 border-transparent h-10 w-10 hover:border-customOrange duration-200 cursor-pointer rounded-full"
+          />
+        </div>
       </div>
-      <div className="flex gap-4 items-center">
-        <button
-          className="relative flex items-center justify-center cursor-pointer hover:bg-slate-100 border border-transparent hover:border-slate-100 h-10 w-10 rounded-full"
-          onClick={() => {
-            setShowCart(!showCart);
-          }}
-        >
-          {cartDetails.itemQuantity > 0 && (
-            <div className="absolute top-1 right-0 bg-customOrange text-white border border-customOrange h-3 w-4 flex items-center justify-center p-1 rounded-lg">
-              <p style={{ fontSize: "10px" }}>{cartDetails.itemQuantity}</p>
-            </div>
-          )}
-          <img src={CartIcon} alt="cart" className="" />
-        </button>
-        <img
-          src={Avatar}
-          alt="avatar"
-          className="border-2 border-transparent h-10 w-10 hover:border-customOrange duration-200 cursor-pointer rounded-full"
-        />
-      </div>
-    </div>
-  );
-});
+    );
+  }
+);
 
 export default Navbar;
